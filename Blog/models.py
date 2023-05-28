f18feffeb249657e6dbbb1e1191ec36d0d95be71
Sairam <sairamramavath01@gmail.com>
2023-05-28 18:24:25 +0530

    Update: Blog

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,36 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from base.models import CustomUser
+
+class Question(models.Model):
+    SYLLABUS_CHOICES = [
+        ('CBSE', 'CBSE'),
+    ]
+
+    CLASS_CHOICES = [
+        ('class-10', 'class-10'),
+    ]
+
+    SUBJECT_CHOICES = [
+        ('Maths', 'Maths'),
+    ]
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    question = models.TextField()
+    syllabus = models.CharField(max_length=50, choices=SYLLABUS_CHOICES)
+    class_field = models.CharField(max_length=50, choices=CLASS_CHOICES)
+    subject = models.CharField(max_length=50, choices=SUBJECT_CHOICES)
+    topic = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    answer_text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.answer_text
--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
 @login_required(login_url='HomePage')
 def learningNetwork(request):
-    return render(request, "Blog/learningNetwork.html", {})
+    questions = Question.objects.all()
+    syllabus_choices = Question.SYLLABUS_CHOICES
+    class_choices = Question.CLASS_CHOICES
+    subject_choices = Question.SUBJECT_CHOICES
+    form_submitted = False
+    if request.method == "POST":
+        question = Question()
+        question.user = request.user 
+        question.question = request.POST['question']
+        question.syllabus = request.POST['syllabus']
+        question.class_field = request.POST['class']
+        question.subject = request.POST['subject']
+        question.topic = request.POST['topic']
+        question.save()
+        form_submitted = True
+        return redirect('learningNetwork')
+    
+    context = {
+        'questions': questions,
+        'syllabus_choices': syllabus_choices,
+        'class_choices': class_choices,
+        'subject_choices': subject_choices,
+        'form_submitted' : form_submitted,
+    }
+    return render(request, "Blog/learningNetwork.html", context)
